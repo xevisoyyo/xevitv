5d6aed80650ee30c21d1fcd04bd5eecf808380e7
Xevi <xevisoyyo@hotmail.com>
2025-11-13 07:30:22 +0100

    actualizar para PC

--- src/pages/teleonline/teleonline.js
+++ src/pages/teleonline/teleonline.js
@@ -1,13 +1,7 @@
-//.media-control-right-panel
-  //.close-btn
-    //.drawer-icon-container
 window.addEventListener("load", ()=>{
-  const closeBtn = document.querySelector(".drawer-icon-container")
-  const parent = closeBtn.parentElement;
-  const parent2 = parent.parentElement;
+  const volumeButton = document.getElementById('volume-button');
+  const fullscreenButton = document.querySelector(".cc-button.media-control-button.media-control-icon");
   setTimeout(()=>{
-    const fullscreen = document.querySelector(".media-control-button.media-control-icon")
-    fullscreen.click()
-  }, 5000)
-  //alert(parent2.innerHTML)
+    fullscreenButton.click();
+  }, 3000);
 })
